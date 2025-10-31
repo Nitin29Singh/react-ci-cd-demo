a513d99c66c9e67d0e29ab782ce648c97a264688
Nitin29Singh <nitin2004singhh29@gmail.com>
2025-10-31 15:19:56 +0530

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>🚀 React CI/CD Pipeline Demo</h1>
+      <p>This app is built, tested, and deployed automatically with GitHub Actions!</p>
+    </div>
+  );
+}
+
+export default App;
